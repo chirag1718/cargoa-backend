7c0101586d9a755ccfb1ebfa38bab34ff9c891cf
Chirag <chiragsonar1718@gmail.com>
2023-06-02 17:41:16 +0530

    added controllers to the routes.

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,8 @@
 import express from "express";
 const router = express.Router();
-import { login, register } from "../controllers/authController.js";
+import { getAllUsers, login, register } from "../controllers/authController.js";
 router.post("/register", register);
 router.post("/login", login);
+router.get("/users", getAllUsers);
 
 export default router;
