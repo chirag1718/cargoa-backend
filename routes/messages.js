b0eeee222c8c29e009fd1019ea3376a5ba2a4f80
Chirag <chiragsonar1718@gmail.com>
2023-06-03 00:21:24 +0530

    uncommented getAllManufacturerMessages route.

--- routes/messages.js
+++ routes/messages.js
@@ -10,8 +10,7 @@ const router = express.Router();
 // Manufacturer
 router.post("/manufacturer/:id", manufacturer);
 router.get("/manufacturer/:id", getManufacturerMessages);
-//  TODO if code breaks this might be the reason
-// router.get("/manufacturer", getAllManufacturerMessages);
+router.get("/manufacturer", getAllManufacturerMessages);
 // Transporter
 router.put("/transporter/:id", transporter);
 router.get("/transporter", getTransporterMessages);
