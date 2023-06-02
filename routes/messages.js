2efc0891030057ab2900178a7c7b52cdc4b120c1
Chirag <chiragsonar1718@gmail.com>
2023-06-02 17:41:49 +0530

    added controllers to the routes.

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   getManufacturerMessages,
+  getTransporterMessages,
   manufacturer,
   transporter,
 } from "../controllers/messageController.js";
@@ -10,5 +11,6 @@ router.post("/manufacturer/:id", manufacturer);
 router.get("/manufacturer/:id", getManufacturerMessages);
 // Transporter
 router.post("/transporter/:id", transporter);
+router.get("/transporter", getTransporterMessages);
 
 export default router;
